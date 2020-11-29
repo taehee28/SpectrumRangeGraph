1f274877f079afbe05ad41be7189d54449e5c721
taehee28 <dev.thk28@gmail.com>
2020-11-29 21:12:28 +0900

    drawing avgPointer & add getter/setters & wirte JavaDoc

--- app/src/main/java/com/thk/spectrumrangegraph/SpectrumRangeGraph.java
+++ app/src/main/java/com/thk/spectrumrangegraph/SpectrumRangeGraph.java
@@ -16,9 +16,24 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 
 /**
+ * SpectrumRangeGraph is showing total range, internal range defined by min and max value, average of internal range.
+ *
+ * 1.
+ * attribute 'srg_graphHeight' exactly means height of Spectrum.
+ * So, actual height of Graph(Spectrum + RangeBox) is 'srg_graphHeight' + 6dp.
+ *
+ * 2.
+ * If you want to specifying Graph's height in XML, have to set the 'android:layout_height' to 'wrap_content'.
+ * OR
+ * If 'android:layout_height' is 'match_parent' or '0dp'(ConstraintLayout) or specific size,
+ * Graph's height is stretched following View's height.
+ *
+ * 3.
+ * If you set the values that effects to View's layout size programmatically, input value must be pixel unit.
+ * (Such as setGraphHeight, setTextSize, setTextMarginTop)
  *
  * @author taehee28
- * @since 2020-11-29
+ * @version 1.0.0 (2020-11-29)
  */
 
 public class SpectrumRangeGraph extends View {
@@ -36,7 +51,7 @@ public class SpectrumRangeGraph extends View {
     private String warningText;
     private boolean showWarningText;
 
-    // defaults
+    // default values
     private final int DEFAULT_MIN_VALUE = 0;
     private final int DEFAULT_MAX_VALUE = 100;
     private final int DEFAULT_GRAPH_HEIGHT = dpToPx(16);
@@ -65,8 +80,6 @@ public class SpectrumRangeGraph extends View {
         d_spectrum = ResourcesCompat.getDrawable(getResources(), R.drawable.srg_bg_spectrum, null);
         d_rangeBox = ResourcesCompat.getDrawable(getResources(), R.drawable.svg_range_box, null);
         d_avgPointer = ResourcesCompat.getDrawable(getResources(), R.drawable.srg_avg_pointer, null);
-
-        warningText = DEFAULT_WARNING_TEXT;
     }
 
     public SpectrumRangeGraph(Context context) {
@@ -87,14 +100,15 @@ public class SpectrumRangeGraph extends View {
         rangeMinValue = attributes.getFloat(R.styleable.SpectrumRangeGraph_srg_rangeMinValue, DEFAULT_MIN_VALUE);
         rangeMaxValue = attributes.getFloat(R.styleable.SpectrumRangeGraph_srg_rangeMaxValue, DEFAULT_MAX_VALUE);
         rangeAvgValue = attributes.getFloat(R.styleable.SpectrumRangeGraph_srg_rangeAvgValue,
-                (rangeMinValue + rangeMaxValue)/2 + rangeMinValue);
+                (rangeMinValue + rangeMaxValue)/2);
 
         graphHeight = attributes.getDimensionPixelSize(R.styleable.SpectrumRangeGraph_srg_graphHeight, DEFAULT_GRAPH_HEIGHT);
         textMarginTop = attributes.getDimensionPixelSize(R.styleable.SpectrumRangeGraph_srg_textMarginTop, DEFAULT_TEXT_MARGIN_TOP);
         textSize = attributes.getDimensionPixelSize(R.styleable.SpectrumRangeGraph_srg_textSize, DEFAULT_TEXT_SIZE);
-        textColor = attributes.getDimensionPixelSize(R.styleable.SpectrumRangeGraph_srg_textColor, DEFAULT_TEXT_COLOR);
+        textColor = attributes.getColor(R.styleable.SpectrumRangeGraph_srg_textColor, DEFAULT_TEXT_COLOR);
 
         warningText = attributes.getString(R.styleable.SpectrumRangeGraph_srg_warningText);
+        if (warningText == null) warningText = DEFAULT_WARNING_TEXT;
         showWarningText = attributes.getBoolean(R.styleable.SpectrumRangeGraph_srg_showWarningText, DEFAULT_SHOW_WARNING_TEXT);
 
         paint_text.setTextSize(textSize);
@@ -105,24 +119,32 @@ public class SpectrumRangeGraph extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        if (showWarningText) {
+            canvas.drawText(warningText, (getWidth() >> 1) - paint_text.measureText(warningText)/2, getHeight() >> 1, paint_text);
+            return;
+        }
+
         rect_spectrum.set(SPECTRUM_PADDING, SPECTRUM_PADDING, getWidth() - SPECTRUM_PADDING, graphHeight + SPECTRUM_PADDING);
         d_spectrum.setBounds(rect_spectrum);
         d_spectrum.draw(canvas);
-
-//        Log.d("thkang", ">> rect_spectrum.width() = " + pxToDp(rect_spectrum.width()));
 
         float graphOffset = (float)pxToDp(rect_spectrum.width()) / (totalMaxValue - totalMinValue);
 
         int rangeBox_left = dpToPx((rangeMinValue - totalMinValue) * graphOffset);
         int rangeBox_right = getWidth() - dpToPx((totalMaxValue - rangeMaxValue) * graphOffset);
         int rangeBox_bottom = rect_spectrum.bottom + SPECTRUM_PADDING;
-
         rect_rangeBox.set(rangeBox_left, 0, rangeBox_right, rangeBox_bottom);
         d_rangeBox.setBounds(rect_rangeBox);
         d_rangeBox.draw(canvas);
 
-        
+        int pointer_left = dpToPx((rangeAvgValue * graphOffset));
+        int pointer_right = dpToPx((rangeAvgValue * graphOffset) + 6);
+        d_avgPointer.setBounds(pointer_left, rect_spectrum.bottom - dpToPx(8), pointer_right, rect_spectrum.bottom);
+        d_avgPointer.draw(canvas);
 
+        int text_y = rect_spectrum.bottom + textMarginTop + textSize;
+        canvas.drawText(String.valueOf(totalMinValue), rect_spectrum.left, text_y, paint_text);
+        canvas.drawText(String.valueOf(totalMaxValue), rect_spectrum.right - paint_text.measureText(String.valueOf(totalMaxValue)), text_y, paint_text);
     }
 
     @Override
@@ -131,7 +153,6 @@ public class SpectrumRangeGraph extends View {
 
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
-//        Log.d("thkang", ">> heightMode = " + heightMode);
         if (heightMode == MeasureSpec.AT_MOST || heightMode == MeasureSpec.UNSPECIFIED) {
             int height = SPECTRUM_PADDING*2 + graphHeight + textMarginTop + textSize;
 
@@ -150,5 +171,117 @@ public class SpectrumRangeGraph extends View {
 
     private int pxToDp(float px) {
         return Util.pxToDp(getResources(), px);
+    }
+
+    public int getTotalMinValue() {
+        return totalMinValue;
+    }
+
+    public void setTotalMinValue(int totalMinValue) {
+        this.totalMinValue = totalMinValue;
+        invalidate();
+    }
+
+    public int getTotalMaxValue() {
+        return totalMaxValue;
+    }
+
+    public void setTotalMaxValue(int totalMaxValue) {
+        this.totalMaxValue = totalMaxValue;
+        invalidate();
+    }
+
+    public float getRangeMinValue() {
+        return rangeMinValue;
+    }
+
+    public void setRangeMinValue(float rangeMinValue) {
+        this.rangeMinValue = rangeMinValue;
+        invalidate();
+    }
+
+    public float getRangeMaxValue() {
+        return rangeMaxValue;
+    }
+
+    public void setRangeMaxValue(float rangeMaxValue) {
+        this.rangeMaxValue = rangeMaxValue;
+        invalidate();
+    }
+
+    public float getRangeAvgValue() {
+        return rangeAvgValue;
+    }
+
+    public void setRangeAvgValue(float rangeAvgValue) {
+        this.rangeAvgValue = rangeAvgValue;
+        invalidate();
+    }
+
+    public int getGraphHeight() {
+        return graphHeight;
+    }
+
+    public void setGraphHeight(int graphHeight) {
+        this.graphHeight = graphHeight;
+        requestLayout();
+    }
+
+    public int getTextMarginTop() {
+        return textMarginTop;
+    }
+
+    public void setTextMarginTop(int textMarginTop) {
+        this.textMarginTop = textMarginTop;
+        invalidate();
+    }
+
+    public int getTextSize() {
+        return textSize;
+    }
+
+    public void setTextSize(int textSize) {
+        this.textSize = textSize;
+        invalidate();
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+        invalidate();
+    }
+
+    public String getWarningText() {
+        return warningText;
+    }
+
+    public void setWarningText(String warningText) {
+        this.warningText = warningText;
+        invalidate();
+    }
+
+    public boolean isShowWarningText() {
+        return showWarningText;
+    }
+
+    public void setShowWarningText(boolean showWarningText) {
+        this.showWarningText = showWarningText;
+        invalidate();
+    }
+
+    public void setTotalValues(int totalMinValue, int totalMaxValue) {
+        this.totalMinValue = totalMinValue;
+        this.totalMaxValue = totalMaxValue;
+        invalidate();
+    }
+
+    public void setRangeValues(float rangeMinValue, float rangeMaxValue, float rangeAvgValue) {
+        this.rangeMinValue = rangeMinValue;
+        this.rangeMaxValue = rangeMaxValue;
+        this.rangeAvgValue = rangeAvgValue;
+        invalidate();
     }
 }
